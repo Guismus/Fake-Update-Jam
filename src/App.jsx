3b49e91e398d38181a384a3480bd6b1a89a99de5
Guismus <guillaume.boudaille@epitech.eu>
2026-07-01 14:48:18 +0200

    Scaffold project boilerplate and structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Chronos Shift Launcher</h1>
+      <p>System initializing...</p>
+    </div>
+  );
+}
+
+export default App;
